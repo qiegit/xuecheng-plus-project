452b368b949c623e130a4d4dfd05a05bb359e1a3
qie <git112@qie.com>
2023-04-02 14:31:21 +0800

    学成parent和base文件

---
+++ src/main/java/com/example/xuechengplusproject/XuechengPlusProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.xuechengplusproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XuechengPlusProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XuechengPlusProjectApplication.class, args);
+    }
+
+}
